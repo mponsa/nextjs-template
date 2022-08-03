713e3e766dd378acd739276a918c7fad28ef67d5
Manuel Ponsa <manuponsa@gmail.com>
2022-08-03 19:50:31 -0300

    feat: implement tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import {
   PrimaryLayout,
   SidebarLayout,
 } from '../components';
-import styles from '../styles/Home.module.css';
+
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className={styles.main}>
-      <h1 className={styles.title}>
+    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
       <CatCard {...mockCatCardProps.base} />
